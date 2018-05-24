e434a7930163621dbe624a58ca57d4d15bd37fcb
anners <annerz@gmail.com>
2018-05-24 14:50:21 -0700

    The Dockerfile used with the quickstart uses python3. I've made changes to the http-server.py to work in a py3 env.

--- http-server.py
+++ http-server.py
@@ -1,25 +1,27 @@
-#!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+#!/usr/bin/env python
+from http.server import BaseHTTPRequestHandler,HTTPServer
 from os import curdir,sep
 
 PORT_NUMBER = 8080
 
 class myHandler(BaseHTTPRequestHandler):
-	
+
 	#Handler for the GET requests
 	def do_GET(self):
 		self.send_response(200)
 		self.send_header('Content-type','image/png')
 		self.end_headers()
-		f = open(curdir + sep + 'logo.png')
-		self.wfile.write(f.read())
+		#f = open(curdir + sep + 'logo.png')
+		#self.wfile.write(f.read())
+		with open(curdir + sep + 'logo.png', 'rb') as file:
+			self.wfile.write(file.read())
+
 		return
 
 try:
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
+	print('Started httpserver on port ' + str(PORT_NUMBER))
 	server.serve_forever()
 
 except KeyboardInterrupt:
 	server.socket.close()
-	
